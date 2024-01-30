32c39a5cee5374b42cc010fa4c5a535cb9ac9ec9
Peiblos <theriseofpauliex@gmail.com>
2024-01-30 20:43:08 +0100

    list viaje

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ViajeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     #[Route('/main', name: 'app_main')]
-    public function index(): Response
+    public function index(ViajeRepository $viajeRepository): Response
     {
+        $viajes = $viajeRepository->findAll();
+        dd($viajes);
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'viajes' => $viajes,
         ]);
     }
 }
