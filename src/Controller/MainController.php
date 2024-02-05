b6c25488721586c7e551a43f009d427327785416
josemanuelcordon <josemanuelcordoncastillo01@gmail.com>
2024-02-05 21:01:02 +0100

    fix: billetes

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\ViajeRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,7 +11,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_main')]
-    public function index(ViajeRepository $viajeRepository): Response
+    public function index(ViajeRepository $viajeRepository, EntityManagerInterface $entityManager): Response
     {
         return $this->render('main/index.html.twig', [
             'venta' => null
